892bb965d096bee40caf53fc721bf202d3c4aff8
Grondi <125816111+Gr0ndi@users.noreply.github.com>
2023-06-07 17:56:15 +0200

    got rid of macro

--- reverse_argv.c
+++ reverse_argv.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#define arg argv[1]
 
 int main(int argc, char* argv[]) {
 
-	if( argc != 2){
+	if(argc != 2){
         printf("Supply 1 argument.\n");
         return -1;
     }
         int length = strlen(argv[1]);
+        char* reversed[length];
+
 		for(int i = length - 1; i != -1; i--){
-			printf("%c", arg[i]); // <-- idk why but it segfaults without that macro lol
+			printf("%c", argv[1][i]);
 		}
 		printf("\n");
+
+        
 	
 }
-
