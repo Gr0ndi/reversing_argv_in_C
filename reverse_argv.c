e7aa02fb0735fe28e4a3cf44f64c858147e4ce4e
Grondi <125816111+Gr0ndi@users.noreply.github.com>
2023-07-14 14:19:44 +0200

    Made the code even simpler.

--- reverse_argv.c
+++ reverse_argv.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(int argc, char* argv[]) {
-
-	if(argc != 2){
-        printf("Supply 1 argument.\n");
-        return -1;
-    }
-        int length = strlen(argv[1]);
-        char* reversed[length];
-
-		for(int i = length - 1; i != -1; i--){
-			printf("%c", argv[1][i]);
-		}
-		printf("\n");
-
-        
+int main(int argc, char** argv){
+	if(argc == 1){
+	  printf("Argument missing\n");
+ 	  return -1;
+	}
 	
+	int arg_len = strlen(argv[1]);
+	for(arg_len != 0; arg_len--;){
+	printf("%c", argv[1][arg_len]);
+	}	
+	printf("\n");
 }
